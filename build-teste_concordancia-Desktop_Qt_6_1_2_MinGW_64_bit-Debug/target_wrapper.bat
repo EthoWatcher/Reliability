bd6da956f4b2757744bfd36c8b37edeef73a68bc
jmarcolan <jamarcolan@gmail.com>
2021-08-27 18:40:21 -0300

    Remover

--- build-teste_concordancia-Desktop_Qt_6_1_2_MinGW_64_bit-Debug/target_wrapper.bat
+++ build-teste_concordancia-Desktop_Qt_6_1_2_MinGW_64_bit-Debug/target_wrapper.bat
@@ -1,10 +1,10 @@
 @echo off
 SetLocal EnableDelayedExpansion
-(set PATH=D:\Qt_new\6.1.2\mingw81_64\bin;!PATH!)
+(set PATH=C:\Qt_new\6.1.2\mingw81_64\bin;!PATH!)
 if defined QT_PLUGIN_PATH (
-    set QT_PLUGIN_PATH=D:\Qt_new\6.1.2\mingw81_64\plugins;!QT_PLUGIN_PATH!
+    set QT_PLUGIN_PATH=C:\Qt_new\6.1.2\mingw81_64\plugins;!QT_PLUGIN_PATH!
 ) else (
-    set QT_PLUGIN_PATH=D:\Qt_new\6.1.2\mingw81_64\plugins
+    set QT_PLUGIN_PATH=C:\Qt_new\6.1.2\mingw81_64\plugins
 )
 %*
 EndLocal
