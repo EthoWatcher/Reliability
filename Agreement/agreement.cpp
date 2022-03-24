01146a738c73fd4725e98a025aa95040a9e0d54f
jmarcolan <jamarcolan@gmail.com>
2022-03-24 17:15:31 -0300

    Arrumando a api da biblioteca

--- Agreement/agreement.cpp
+++ Agreement/agreement.cpp
@@ -4,19 +4,24 @@ Agreement::Agreement()
 {
 }
 
-QString Agreement::generate_report(QString path)
+QString Agreement::generate_report(QString path,
+                                   std::vector<int> etrografia_1,
+                                   std::vector<int> etrografia_2,
+                                   std::vector<int> catalogo,
+                                   QList<QString> cata_name
+                                   )
 {
-    qDebug() << "report";
+//    qDebug() << "report";
 
-    std::vector<int> etrografia_1  = {0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2};
-    std::vector<int> etrografia_2  = {1, 1, 1, 1, 0, 0, 0, 0, 2, 2, 2, 2, 2};
-    std::vector<int> catalogo  = {0, 1, 2};
-    QList<QString> cata_name = {"swimming",
-                                 "climbimg",
-                                 "Immobility"};
-//    0 - swimming
-//    1 - climbimg
-//    2 - Immobility
+//    std::vector<int> etrografia_1  = {0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2};
+//    std::vector<int> etrografia_2  = {1, 1, 1, 1, 0, 0, 0, 0, 2, 2, 2, 2, 2};
+//    std::vector<int> catalogo  = {0, 1, 2};
+//    QList<QString> cata_name = {"swimming",
+//                                 "climbimg",
+//                                 "Immobility"};
+////    0 - swimming
+////    1 - climbimg
+////    2 - Immobility
     Relatorio_paper relatorio = Relatorio_paper(etrografia_1,
                                                 etrografia_2,
                                                 catalogo,
--- Agreement/agreement.h
+++ Agreement/agreement.h
@@ -7,7 +7,12 @@ class AGREEMENT_EXPORT Agreement
 {
 public:
     Agreement();
-    QString generate_report(QString path);
+    QString generate_report(QString path,
+                            std::vector<int> etrografia_1,
+                            std::vector<int> etrografia_2,
+                            std::vector<int> catalogo,
+                            QList<QString> cata_name
+                            );
 };
 
 #endif // AGREEMENT_H
